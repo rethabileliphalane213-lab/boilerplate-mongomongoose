5e44dcc08a95941654343cf8772f851297bfad9a
rethabileliphalane213-lab <rethabileliphalane213@gmail.com>
2026-04-15 11:49:15 +0000

    create model

--- myApp.js
+++ myApp.js
@@ -3,8 +3,22 @@ console.log("MONGO_URI:", process.env.MONGO_URI);;
 const mongoose=require("mongoose")
 
 mongoose.connect(process.env.MONGO_URI ,{ useNewUrlParser: true, useUnifiedTopology: true });
+const personSchema=new mongoose.Schema({
 
-let Person;
+  name:{
+    type: String,
+    required:true
+  },
+  age:{
+    type: Number
+  },
+  favoriteFoods:{
+    type: [String]
+  }
+
+})
+
+let Person=mongoose.model("Rethabile",personSchema)
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
